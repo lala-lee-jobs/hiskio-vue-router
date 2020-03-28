d0ca9f20745d3b3cd3a0c5d3ecaab00815677390
拉拉醬 <lala.lee.goo@gmail.com>
2020-03-28 11:10:41 +0800

    巢狀路由，設定子路由

--- src/About.vue
+++ src/About.vue
@@ -1,3 +1,16 @@
 <template>
-    <h1>About</h1>
+    <div>
+        <h1>About</h1>
+        <router-link to="/about/us">Us</router-link>
+        <router-link to="/about/you">You</router-link>
+        <router-view></router-view>
+    </div>
 </template>
+<script>
+export default {
+    
+}
+</script>
+<<style lang="scss">
+    
+</style>
---
+++ src/AboutYou.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2>You</h2>
+</template>
